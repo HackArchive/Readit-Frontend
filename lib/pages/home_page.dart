2c361078aa743a246a879f79f6f67fb003d9b2f9
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-11-06 15:13:06 +0530

    User Page Improvements

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -40,7 +40,6 @@ class _HomePageState extends State<HomePage> {
   }
 
   userIconTapped(BuildContext context) {
-    context.read<UserStore>().logout();
     Navigator.pushReplacementNamed(context, Routes.profile);
   }
 
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,15 +1,56 @@
+import 'package:clock_hacks_book_reading/models/user_model.dart';
+import 'package:clock_hacks_book_reading/store/user_store.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    User? user = context.watch<UserStore>().currentUser!;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Profile'),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Container(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              CircleAvatar(
+                radius: 60,
+                child: Text(
+                  user.name.characters.first,
+                  style: const TextStyle(
+                    fontSize: 60,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                user.name,
+                style: const TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                user.email,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.normal,
+                  fontStyle: FontStyle.normal,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
